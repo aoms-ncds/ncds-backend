f40ecfa9f9bbdb3f8cb1fdf3cd46651a2d025f4b
Shibin Sk <shibinskombanal@gmail.com>
2026-08-08 23:41:29 +0530

    cmt

--- src/extras/Firebase/firebaseAdmin.ts
+++ src/extras/Firebase/firebaseAdmin.ts
@@ -1,12 +1,12 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../../pro910-iet-firebase-adminsdk-fbsvc-617ec747ac.json';
+// import serviceAccount from '../../../pro910-iet-firebase-adminsdk-fbsvc-617ec747ac.json';
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    projectId: 'pro910-iet',
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//     projectId: 'pro910-iet',
+//   });
+// }
 
 // We can directly export admin to access all services (auth, messaging, storage, etc.)
 export default admin;
