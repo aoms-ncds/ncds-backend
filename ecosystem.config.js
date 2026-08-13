04d33c91c5855c6c002187ff61bf56940efa1447
Shibin Sk <shibinskombanal@gmail.com>
2026-08-13 23:20:47 +0530

    cc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   apps: [{
-    name: 'ncds-app',
-    script: './server.js', // <-- change to your actual entry file
-    instances: 'max', // uses all available cores
+    name: 'ncds-server',
+    script: './dist/bundle.js', // <-- confirm this matches your actual entry file
+    instances: 'max',
     exec_mode: 'cluster',
-    port: 8003,
+    env: {
+      PORT: 8002,
+    },
   }],
 };
